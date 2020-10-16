b46512ca787115df376ebd2f6aafc1ba7c9b150f
Alfan Jauhari <hi.alfanj@gmail.com>
2020-10-16 11:27:55 +0700

    Add PWA

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
+const withPlugins = require('next-compose-plugins');
 const withImage = require('next-optimized-images');
 const withMdx = require('next-mdx-enhanced');
+const withPwa = require('next-pwa');
 const rehypePrism = require('@mapbox/rehype-prism');
-const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins(
   [
     [withImage],
+    [
+      withPwa,
+      {
+        pwa: {
+          dest: 'public',
+          register: true,
+          scope: '/',
+          sw: 'sw.js',
+          disable: process.env.NODE_ENV === 'development'
+        }
+      }
+    ],
     withMdx({
       layoutPath: 'layouts',
       defaultLayout: true,
